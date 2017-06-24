ffd09458aafc82fabc9dcefd4116fe7bec8ac31a
snollygolly <snollygolly@gmail.com>
2017-06-24 13:25:49 -0500

    added ability to wait if progress bar hasn't moved
    also added improved logging

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let app;
 let listing = null;
 let isAutomated = false;
 let block = false;
+let waiting = false;
+let hackProgress = 0;
 let wordLoop = null;
 let minerLoop = null;
 let minerStatus = [
@@ -36,6 +38,7 @@ const db = "https://raw.githubusercontent.com/snollygolly/sourceio-automation/ma
 const message = "papa bless, one love /r/javascript";
 const wordFreq = 1250;
 const mineFreq = 3000;
+const blockFreq = 5000;
 const minerLevel = 20;
 
 app = {
@@ -47,23 +50,23 @@ app = {
 	},
 
 	exportListing: () => {
-		console.log(JSON.stringify(listing, null, 2));
+		log(JSON.stringify(listing, null, 2));
 	},
 
 	automate: () => {
 		// first check the windows are open, and open them if they aren't
 		if ($("#player-list").is(":visible") === false) {
-			console.error("! Target list must be open");
+			log("! Target list must be open");
 			$("#desktop-list").children("img").click();
 		}
 		if ($("#window-shop").is(":visible") === false) {
-			console.error("! Black market must be open");
+			log("! Black market must be open");
 			$("#desktop-shop").children("img").click();
 		}
 		isAutomated = true;
 		// start by getting the first target in the list
 		const targetName = $("#player-list").children("tr").eq(0)[0].innerText;
-		console.log(`* Now attacking ${targetName}`);
+		log(`. Now attacking ${targetName}`);
 		// click it, and then hack, and then port b
 		$("#player-list").children("tr").eq(0)[0].click();
 		$("#window-other-button").click();
@@ -71,8 +74,8 @@ app = {
 		const portStyle = $("#window-other-port2").attr("style");
 		if (portStyle.indexOf("opacity: 1") === -1) {
 			// this port costs too much, let's wait a bit
-			console.log("* Hack too expensive, waiting");
-			setTimeout(app.automate, 1000);
+			log("* Hack too expensive, waiting");
+			setTimeout(app.automate, blockFreq);
 			return;
 		}
 		$("#window-other-port2").click();
@@ -95,6 +98,22 @@ app = {
 				app.restart();
 				return;
 			}
+			// if we're waiting on the progress bar to move...
+			if (waiting === true) {
+				const newHackProgress = parseHackProgress($("#progressbar-firewall-amount").attr("style"));
+				// check to see if it's new
+				if (hackProgress === newHackProgress) {
+					// the bar hasn't moved
+					log("* Progress bar hasn't moved, waiting");
+					return;
+				} else {
+					// the bar has moved
+					hackProgress = newHackProgress;
+					waiting = false;
+				}
+			}
+			// actually do the word stuff
+			waiting = true;
 			app.go();
 		},
 		miner: () => {
@@ -120,7 +139,7 @@ app = {
 
 	stop: () => {
 		if (wordLoop === null && minerLoop === null) {
-			console.log("! No loops to stop");
+			log("! No loops to stop");
 			return;
 		}
 		isAutomated = false;
@@ -128,23 +147,22 @@ app = {
 		clearInterval(wordLoop);
 		clearInterval(minerLoop);
 		wordLoop = null;
-		console.log("* Stopped loop");
+		log("* Stopped loop");
 	},
 
 	exportListing: () => {
-		console.log(JSON.stringify(listing, null, 2));
+		log(JSON.stringify(listing, null, 2));
 	},
 
 	go: () => {
-		console.log("* Running 'go'");
 		const wordLink = $(".tool-type-img").prop("src");
 		if (listing.hasOwnProperty(wordLink) === true) {
 			const word = listing[wordLink];
-			console.log(`* Found word: [${word}]`);
+			log(`. Found word: [${word}]`);
 			app.submit(word);
 			return;
 		}
-		console.log("* Not seen, trying OCR...");
+		log("* Not seen, trying OCR...");
 		app.ocr(wordLink);
 	},
 
@@ -168,16 +186,27 @@ app = {
 		}).done((data) => {
 			const word = String(data["ParsedResults"][0]["ParsedText"]).trim().toLowerCase();
 			if (word.length > 3) {
-				console.log(`* Got data: [${word}]`);
+				log(`. Got data: [${word}]`);
 				$("#tool-type-word").val(word);
 				if (isAutomated === true) {
 					app.learn(word);
 					block = false;
 				}
 			} else {
-				console.log("! OCR failed");
+				log("* OCR failed");
 				app.restart();
 			}
 		});
 	}
 };
+
+function parseHackProgress(progress) {
+	// remove the %;
+	const newProgress = progress.slice(0, -2);
+	const newProgressParts = newProgress.split("width: ");
+	return parseInt(newProgressParts.pop());
+}
+
+function log(message) {
+	console.log(`:: ${message}`);
+}
