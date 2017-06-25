eca9e6040c23c00c26ec5304bbe409f9b006a5ad
bozoweed <bozoweed@gmail.com>
2017-06-25 03:33:05 +0200

    update

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ let maxStats = {
 };
 const firewalls = ["1", "2", "3"];
 const ocrApiKey = "XXX";
-const db = "https://raw.githubusercontent.com/snollygolly/sourceio-automation/master/db.json";
+const db = "https://raw.githubusercontent.com/bozoweed/sourceio-automation/master/db.json";
 let message = "papa bless, one love /r/javascript";
 let wordFreq = 1250;
 let mineFreq = 3000;
