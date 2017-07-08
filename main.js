bbe8cc03a4f35db4b9b0dd4e587e998f4d454e43
abxu <shyw59@gmail.com>
2017-07-08 18:14:18 +0200

    Fixed URL

--- main.js
+++ main.js
@@ -322,7 +322,7 @@ app = {
 
 	go: () => {
 		const wordLink = $(".tool-type-img").prop("src");
-		if (wordLink !== "http://s0urce.io/client/img/words/template.png") {
+		if (!wordLink.endsWith("s0urce.io/client/img/words/template.png")) {
 			if (listing.hasOwnProperty(wordLink) === true) {
 				const word = listing[wordLink];
 				log(`. Found word: [${word}]`);
